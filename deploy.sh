19f110f34e33841bc94c3349e4df182adc84bee3
foreverfl <forever_fl@naver.com>
2023-08-14 15:37:54 +0900

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 리포지토리 경로로 이동
-cd /home/ec2-user/./-Web-My-Study-Materials
+cd /home/ec2-user/-Web-My-Study-Materials
 
 # 원격 리포지토리에서 변경 사항 가져오기
 git fetch
@@ -9,10 +9,11 @@ git fetch
 # 로컬과 원격의 차이점 확인
 DIFF=$(git diff HEAD origin)
 
-# 차이점이 있다면 변경 사항 적용
 if [ "$DIFF" != "" ]
 then
     git pull
+    echo "$(date) : Git pulled successfully" >> /var/log/my_cron.log # 로그에 기록
     # 필요한 경우 서비스 재시작 등의 명령어 추가
     systemctl restart nginx
+    echo "$(date) : Nginx restarted" >> /var/log/my_cron.log # 로그에 기록
 fi
